1da9c9c0b3886784de3f03e93906a49baec4ac35
James Marshall <jameswhmarshallactions@gmail.com>
2021-08-16 22:53:18 +1000

    commit before submission

--- src/Components/SearchPageResults.js
+++ src/Components/SearchPageResults.js
@@ -3,22 +3,6 @@ import React from 'react'
 
 const SearchPageResults = (props) => {
 
-  // TODO: You may need to add a method to filter books that are already in state on the main page
-
-//   const searchResults = props.foundBooks.map(book => {
-
-//     return (<Book 
-//         key={book.id}
-//         id={book.id} 
-//         image={book.imageLinks ? book.imageLinks.smallThumbnail : null}
-//         title={book.title}
-//         authors={book.authors}
-//         shelf={book.shelf ? book.shelf : 'none'}
-//         bookShelfHandler={props.bookShelfHandler}
-//         bookListHandler={props.bookListHandler}
-//         />)
-// })
-
     return (
         <div className="search-books-results">
         <ol className="books-grid">{props.foundBooks}</ol>
