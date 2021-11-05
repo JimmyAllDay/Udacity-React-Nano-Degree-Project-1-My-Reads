f053ebd5fb97200ca7c254eb2e1cb0d5dc764c32
James Marshall <jameswhmarshallactions@gmail.com>
2021-11-05 16:01:40 +1100

    updated to hashrouter

---
+++ build/precache-manifest.4c59131543667ed7cd8120a2d8d43a3b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "04857298d470615784b2",
+    "url": "/book-sprout/static/css/main.c208c153.chunk.css"
+  },
+  {
+    "revision": "04857298d470615784b2",
+    "url": "/book-sprout/static/js/main.04857298.chunk.js"
+  },
+  {
+    "revision": "dcae7e15756deebb8092",
+    "url": "/book-sprout/static/js/1.dcae7e15.chunk.js"
+  },
+  {
+    "revision": "5793b9afe9e2816241eb",
+    "url": "/book-sprout/static/js/runtime~main.5793b9af.js"
+  },
+  {
+    "revision": "512d9f3be16cfa048b3617d60a65b58b",
+    "url": "/book-sprout/static/media/sprout-logo.512d9f3b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/book-sprout/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/book-sprout/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/book-sprout/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "65d70c27b1c1e202c75a0a7cbe5d60fc",
+    "url": "/book-sprout/index.html"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-
-<BrowserRouter>
+  <HashRouter>
     <App />
-</BrowserRouter>, document.getElementById('root'))
+  </HashRouter>,
+  document.getElementById("root")
+);
