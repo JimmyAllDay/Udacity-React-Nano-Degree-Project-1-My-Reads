b41f0188a045ca924ecdd07928b21b8445fec281
James Marshall <jameswhmarshallactions@gmail.com>
2021-11-05 12:24:59 +1100

    Updates

---
+++ precache-manifest.d9de983f9f1221c47d108d21b570aef8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2089ba2ea1101e2f55b9",
+    "url": "/book-sprout/static/css/main.c208c153.chunk.css"
+  },
+  {
+    "revision": "2089ba2ea1101e2f55b9",
+    "url": "/book-sprout/static/js/main.2089ba2e.chunk.js"
+  },
+  {
+    "revision": "7a8e91c57860450a490f",
+    "url": "/book-sprout/static/js/1.7a8e91c5.chunk.js"
+  },
+  {
+    "revision": "5793b9afe9e2816241eb",
+    "url": "/book-sprout/static/js/runtime~main.5793b9af.js"
+  },
+  {
+    "revision": "512d9f3be16cfa048b3617d60a65b58b",
+    "url": "/book-sprout/static/media/sprout-logo.512d9f3b.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/book-sprout/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/book-sprout/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/book-sprout/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "f3b62e8cfa57b5b2f661a8b6ce36c6d9",
+    "url": "/book-sprout/index.html"
+  }
+];
